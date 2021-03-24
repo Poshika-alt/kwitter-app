11e1ad213ad54b81e23b285ff64df462170c9d0e
Poshika-alt <76203042+Poshika-alt@users.noreply.github.com>
2021-03-24 22:02:54 +0530

    Add files via upload

---
+++ kwitter_app.js
@@ -0,0 +1,12 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD3J2KS8GAoJkvJvTPSMUD2r82tZDLgoJE",
+    authDomain: "kwitter-project-96f75.firebaseapp.com",
+    databaseURL: "https://kwitter-project-96f75-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-96f75",
+    storageBucket: "kwitter-project-96f75.appspot.com",
+    messagingSenderId: "425203922126",
+    appId: "1:425203922126:web:f8bc22fe9cb39525d78c9f",
+    measurementId: "G-1488EL1ZY8"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
